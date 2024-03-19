b5146e566b4f8af414935681ce518f02a1ea3eee
Mrlion <mrlion4202@gmail.com>
2024-03-19 19:27:32 +0000

    done for today, still some errors

--- Server/server.lua
+++ Server/server.lua
@@ -1,16 +1,22 @@
-SetConvarReplicated("sv_maxclients", "true")
 local Config = require('../config')
+SetConvarReplicated("sv_maxclients", "true")
 
 if Config.Updater then
     PerformHttpRequest("https://raw.githubusercontent.com/L0stedMrlion/versions/master/LionRPC", function(err, Version, headers)
+        if Version then
+            local ScriptVersion = GetResourceMetadata(GetCurrentResourceName(), "version")
+            Version = tonumber(Version)
 
-        local ScriptVersion = GetResourceMetadata(GetCurrentResourceName(), "version")
-    
-        if GetResourceMetadata(GetCurrentResourceName(), "version") == Version then
-            print("^2[INFO] LionRPC is running on latest")
+            if tonumber(ScriptVersion) == Version then
+                print("^2[INFO] LionRPC is running on the latest version")
+            else
+                print("^3[UPDATE] LionRPC has a new version! (" .. ScriptVersion .. " -> " .. Version .. ")")
+                print("^3[UPDATE] https://github.com/L0stedMrlion/LionRPC")
+            end
         else
-            print("^3[UPDATE] LionRPC have a new version! ("..ScriptVersion.. " -> " ..Version..")")
-            print("^3[UPDATE] https://github.com/L0stedMrlion/LionRPC")
+            print("^1[ERROR] Failed to retrieve version information.")
         end
     end)
+else
+    print("^1[WARNING] Updater is disabled in the Config.")
 end
